7abc4c512d0b1135095e6f8b0aea9857d681beee
Chen <cyu0@walmartlabs.com>
2020-03-19 17:38:10 -0700

    add dedicated receiver go routine in printwork caller

--- topics/go/concurrency/patterns/task/main/main.go
+++ topics/go/concurrency/patterns/task/main/main.go
@@ -51,11 +51,20 @@ func main() {
 
 	// Create a task pool.
 	t := task.New(routines)
+	allres := make(chan string, 100)
 
 	var wg sync.WaitGroup
 	wg.Add(len(printnametasks))
 
+	// go routine to get results from allres channel
+	go func() {
+		for res := range allres {
+			fmt.Println(res)
+		}
+	}()
+
 	// Iterate over the slice of names.
+	// Submit workload parallel to task object with Do() api
 	for _, name := range printnametasks {
 
 		// Create a namePrinter and provide the
@@ -70,14 +79,20 @@ func main() {
 			// returns, we know it is being handled.
 			t.Do(np)
 			result := <-t.Res
-			fmt.Println(result)
+			//fmt.Println(result)
+			allres <- result
+
+			// Once we get the result from task object,
+			// we know this task is done
 			wg.Done()
 		}()
 	}
 
+	// wait for all the printwork we sent to task are done
 	wg.Wait()
 
 	// Shutdown the task pool and wait for all existing work
 	// to be completed.
 	t.Shutdown()
+	close(allres)
 }
